e3b9f9651cf0b1251c1c5d1e8e5c7f27924f77e4
DESKTOP-EA3UTGO\ujjwal <ujjwalkumargupta4u@gmail.com>
2018-04-25 10:33:31 +0530

    added files to docs folder

---
+++ docs/code/layouts/tutorial_layout.vue
@@ -0,0 +1,52 @@
+<template>
+<v-layout>
+    <v-flex l2 class="hidden-md-and-down">
+<ul>
+     <li class="search">
+                                <div class="search-wrapper card">
+                                    <input id="txtSearch" type="text" placeholder="Search">
+                                    <i class="material-icons">search</i>
+                                    <div class="search-results"></div>
+                                </div>
+    </li>
+    <li v-for="link in linkList" :key="link">
+        <a href="#">{{link.text}}</a>
+    </li>
+</ul>
+    </v-flex>
+    <v-flex id="divTutorialContent" sm12 md9 l7 xl6 class="margin-left-15px">
+    
+    </v-flex>
+</v-layout>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export interface ITutorialLink {
+  text: string;
+  url: string;
+}
+export default class Tutorial extends Vue {
+  linkList: ITutorialLink[] = [];
+  constructor() {
+    super();
+    this.linkList = this.getLinks();
+    debugger;
+    console.log(this.linkList);
+  }
+
+  getLinks() {
+    return [
+      {
+        text: "Get Started",
+        url: "tutorial"
+      }
+    ] as ITutorialLink[];
+  }
+}
+</script>
+
+
+<script src="~/assets/scripts/tutorial.js"></script>
+<style scoped>
+@import url("~/assets/css/tutorial.css");
+</style>
